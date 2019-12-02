a234b4e95b2ef6bc0eae5a3c3cdf924d79473c77
julczka <56249914+julczka@users.noreply.github.com>
2019-12-02 20:58:22 +0100

    model nie dziala

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import vb from 'vue-babylonjs';
+Vue.use(vb);
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <Scene>
+  <Camera type="arcRotate" :radius="7.5" :beta="Math.PI / 3"></Camera>
+  <HemisphericLight></HemisphericLight>
+  <Asset src="https://www.babylonjs-playground.com/scenes/skull.babylon" :scaling="[0.02, 0.02, 0.02]" :position="[4, 0.5, 0]"></Asset>
+  <Asset src="https://www.babylonjs-playground.com/scenes/Buggy/glTF/buggy.gltf" :scaling="[0.02, 0.02, 0.02]" :position="[1, 0, -1]" :rotation="[0, Math.PI / 2, 0]"></Asset>
+  <Asset src="https://www.babylonjs-playground.com/scenes/StanfordBunny.obj" :scaling="[7.5, 7.5, 7.5]" :position="[-4, -0.25, -0.5]" :rotation="[0, Math.PI, 0]"></Asset>
+  
+  <Asset src="../assets.hammer.glb" :scaling="[0.02, 0.02, 0.02]" :position="[-2, 0, -0.5]"></Asset>
+</Scene>
 </template>
